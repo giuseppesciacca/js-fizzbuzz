926d4d01fc6b930f74de7f7e1f4527b64d56c3b9
giuseppesciacca <sciaccagi@gmail.com>
2023-03-10 16:26:26 +0100

    [add] if statement in list

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,9 +15,19 @@ per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”. */
 
 const ulEl = document.querySelector('ul');
 
-for (let i = 0; i < 100; i++) {
+for (let i = 1; i <= 100; i++) {
 
-    const liEl = `<li> ${i} </li>`;
-    ulEl.insertAdjacentHTML("beforeend", liEl);
-    console.log(i)
+    if (i % 3 == 0) {
+        let liEl = `<li> ${i} Fizz </li>`;
+        ulEl.insertAdjacentHTML("beforeend", liEl);
+    } else if (i % 5 == 0) {
+        let liEl = `<li> ${i} Buzz </li>`;
+        ulEl.insertAdjacentHTML("beforeend", liEl);
+    } else if (i % 3 == 0 && i % 5 == 0) {
+        let liEl = `<li> ${i} FizzBuzz </li>`;
+        ulEl.insertAdjacentHTML("beforeend", liEl);
+    } else {
+        let liEl = `<li> ${i} </li>`;
+        ulEl.insertAdjacentHTML("beforeend", liEl);
+    }
 };
